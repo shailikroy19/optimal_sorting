d0e2a89db5be0d803ca27d4178276772b94f8598
RishyaniBhattacharya <iamrishyani@gmail.com>
2021-10-01 09:55:48 +0530

    update inpur

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -77,7 +77,7 @@ void printArray(int A[], int size)
 // Driver code
 int main()
 {
-	int arr[] = { 12, 11, 13, 5, 6, 7 };
+	int arr[] = { 10, 20,13, 5, 6, 7 };
 	auto arr_size = sizeof(arr) / sizeof(arr[0]);
 
 	cout << "Given array is \n";
